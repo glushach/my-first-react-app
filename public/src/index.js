cab7b60fde146c836ec7c1a092f12d0ce9c5aec8
Alex <shoppingplaneta@gmail.com>
2021-10-03 20:50:47 +0300

    init

--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
 import './index.css';
-import App from './App';
-import {Header} from "./App";
 
 ReactDOM.render(
-  <App/>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+    return <h2>Hello World</h2>
+};
+
+const Field = () => {
+    return <input placeholder='Type here' type="text"/>
+}
+
+function Btn() {
+    // const text = 'Log in';
+    const res = () => {
+        return 'Log in';
+    }
+    return <button>{res()}</button>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
