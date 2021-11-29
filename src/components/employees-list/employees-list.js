f85a976eb84d7f2bafea245cda0361c9e924ebd0
Alex <shoppingplaneta@gmail.com>
2021-11-29 10:47:31 +0200

    added property key for components

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,11 +3,15 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 const EmployeesList = ({data}) => {
+
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+
         return (
-            <EmployeesListItem {...item}/>
+            <EmployeesListItem key={id} {...itemProps}/>
         )
     });
+
     return (
         <ul className="app-list list-group">
             {elements}
